2a0385328c85c51b9198e737eb1629127e0b43de
Mary Lang <mcl86@cornell.edu>
2019-11-01 09:40:17 -0600

    Complete US2

--- spec/models/student_course_spec.rb
+++ spec/models/student_course_spec.rb
@@ -8,4 +8,13 @@ describe StudentCourse do
     it { should belong_to :student }
     it { should belong_to :course }
   end
+  describe 'attributes' do
+    it 'has attributes' do
+      course = Course.create(name: 'Biology')
+      student = Student.create(name: 'Olivia')
+      sc_1 = course.student_courses.create(student: student, grade: 3.5)
+
+      expect(sc_1.grade).to eq(3.5)
+    end 
+  end
 end
